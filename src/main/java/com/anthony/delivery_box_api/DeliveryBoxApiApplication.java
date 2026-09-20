a2a24a57dbf56a85ebf7116b2f8f6dfbf4aa7b71
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2026-09-20 21:35:16 +0100

    Initial project scaffold with Spring Web, JPA, H2, and Validation dependencies

---
+++ src/main/java/com/anthony/delivery_box_api/DeliveryBoxApiApplication.java
@@ -0,0 +1,13 @@
+package com.anthony.delivery_box_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryBoxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryBoxApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anthony/delivery_box_api/DeliveryBoxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthony.delivery_box_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBoxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
