f239a1866f03333663c7ae105b88917759d6e3d3
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2026-09-21 05:48:49 +0100

    Remove unused HttpStatus import

--- src/main/java/com/anthony/delivery_box_api/GlobalExceptionHandler.java
+++ src/main/java/com/anthony/delivery_box_api/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.anthony.delivery_box_api;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
