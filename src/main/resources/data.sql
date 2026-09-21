323d3ed5f73cecccebaa03d87767284b85ef962f
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2026-09-21 04:50:12 +0100

    Add preloaded sample box data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO box (txref, weight_limit, battery_capacity, state) VALUES ('BOX-001', 500, 100, 'IDLE');
+INSERT INTO box (txref, weight_limit, battery_capacity, state) VALUES ('BOX-002', 300, 15, 'IDLE');
+INSERT INTO box (txref, weight_limit, battery_capacity, state) VALUES ('BOX-003', 400, 80, 'DELIVERING');
